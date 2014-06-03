9c9371622b108dc8f5f71e6e12d198958755f064
Sam Dev <dev.samer@hotmail.com>
2014-06-03 00:54:27 -0700

    Declare Application Class

---
+++ src/Core/Application.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sam Dev
+ * Date: 6/3/14
+ * Time: 12:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace PHPJet\Core;
+
+
+class Application {
+    public function __construct()
+    {
+        echo "Say Hello";
+    }
+}
